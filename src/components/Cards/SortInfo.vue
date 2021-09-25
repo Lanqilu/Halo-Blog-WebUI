611086650e1a6998511d43b1ce226565d231580b
Lanqilu <885240677@qq.com>
2021-09-26 01:19:32 +0800

    feat: 简单的点赞样式

---
+++ src/components/Cards/SortInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{ state.sort }}
+  </div>
+</template>
+
+<script>
+import {getSortByUserId} from "../../api";
+import {useStore} from "vuex";
+
+export default {
+  name: "SortInfo",
+
+  setup() {
+    const store = useStore();
+    const userId = store.getters.getUser.id;
+    console.log(userId);
+    let state = {
+      sort: null,
+    }
+
+
+    state.sort = getSortByUserId(userId);
+    let sortDate = state.sort.date;
+    console.log(sortDate);
+
+
+    console.log(state.sort);
+
+    return {
+      state
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
